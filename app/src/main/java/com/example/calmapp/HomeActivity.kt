0904dd7608932f71d8bd2b4d9563205fe6a5d3a6
Horizon733 <dishantgandhi733@gmail.com>
2021-05-21 23:24:28 +0530

    added fab menu with animation

--- app/src/main/java/com/example/calmapp/HomeActivity.kt
+++ app/src/main/java/com/example/calmapp/HomeActivity.kt
@@ -2,10 +2,70 @@ package com.example.calmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.calmapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottomToTop: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_to_top_anim) }
+    private val fromTopToBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_top_to_bottom_anim) }
+    private lateinit var homeBinding: ActivityHomeBinding
+    private var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        homeBinding.moreFab.setOnClickListener {
+            onMenuButtonClicked()
+        }
+        homeBinding.homeFab.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(homeBinding.homeContainer.id,HomeFragment(),"Home").commit()
+        }
+        homeBinding.calendarFab.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(homeBinding.homeContainer.id,HomeFragment(),"Calendar").commit()
+        }
+        homeBinding.settingsFab.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(homeBinding.homeContainer.id,HomeFragment(),"Settings").commit()
+        }
+    }
+
+    private fun onMenuButtonClicked() {
+        setAnimation(clicked)
+        setVisibility(clicked)
+        clicked = !clicked
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked){
+            homeBinding.settingsFab.startAnimation(fromBottomToTop)
+            homeBinding.homeFab.startAnimation(fromBottomToTop)
+            homeBinding.calendarFab.startAnimation(fromBottomToTop)
+            homeBinding.moreFab.startAnimation(rotateOpen)
+            homeBinding.moreFab.setImageResource(R.drawable.ic_baseline_close_24)
+        }else{
+            homeBinding.settingsFab.startAnimation(fromTopToBottom)
+            homeBinding.homeFab.startAnimation(fromTopToBottom)
+            homeBinding.calendarFab.startAnimation(fromTopToBottom)
+            homeBinding.moreFab.startAnimation(rotateClose)
+            homeBinding.moreFab.setImageResource(R.drawable.ic_more)
+        }
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked){
+            homeBinding.settingsFab.visibility = View.VISIBLE
+            homeBinding.homeFab.visibility = View.VISIBLE
+            homeBinding.calendarFab.visibility = View.VISIBLE
+
+        }else{
+            homeBinding.settingsFab.visibility = View.INVISIBLE
+            homeBinding.homeFab.visibility = View.INVISIBLE
+            homeBinding.calendarFab.visibility = View.INVISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/calmapp/MainActivity.kt
+++ app/src/main/java/com/example/calmapp/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.calmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
