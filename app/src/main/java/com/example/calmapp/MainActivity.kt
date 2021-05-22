7f55c84cf7a96cbbbdc846887c76311ba2741c31
Biren Nayak <nayakbiren18@gmail.com>
2021-05-22 14:58:03 +0530

    updated color Settings

--- app/src/main/java/com/example/calmapp/MainActivity.kt
+++ app/src/main/java/com/example/calmapp/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.calmapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = findViewById<FrameLayout>(R.id.fragment)
+        supportFragmentManager.beginTransaction().add(fragment.id,SettingsFragment(),"Settings").commit()
+
 
     }
 }
--- app/src/main/java/com/example/calmapp/SettingsFragment.kt
+++ app/src/main/java/com/example/calmapp/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.calmapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 
 /**
  * A simple [Fragment] subclass.
@@ -21,7 +23,29 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    fun darkModeToggler(){
+        val isNightTheme = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (isNightTheme) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                activity?.finish()
+                activity?.overridePendingTransition(0, 0)
+//                startActivity(intent)
+                activity?.overridePendingTransition(0, 0)
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                activity?.finish()
+                activity?.overridePendingTransition(0, 0)
+//                startActivity(intent)
+                activity?.overridePendingTransition(0, 0)
+            }
+
+        }
     }
 }
